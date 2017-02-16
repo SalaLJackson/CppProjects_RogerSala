5985359b1917409b4d6584f1f96340c5c7aa3dda
rogersalamaja <rogersala1997@gmail.com>
2017-02-16 11:21:48 +0100

    Començat

---
+++ Random​EnemyGenerator/RandomEnemyGenerator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+enum EnemyType { zombie, ghost, vampire, witch };
+
+const int enemyNum = 5;
+
+struct Enemy
+{
+	EnemyType type;
+	int health;
+	string name;
+};
+
+#include "RandomEnemyGenerator.h"
+using namespace std;
+
+void main()
+{
+	Enemy arr[enemyNum];
+}
+
+Enemy CreateRandomEnemy()
+{
+	EnemyType en;
+	rand() % 4;
+	return en;
+}
